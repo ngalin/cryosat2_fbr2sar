45c930ea419a380e558bdcd460b6a9b8a6c8e6fa
Natalia Galin <natalia.galin@gmail.com>
2013-05-06 11:43:09 -0300

    Update FUNC_OCOG_SARv2.m

--- FUNC_OCOG_SARv2.m
+++ FUNC_OCOG_SARv2.m
@@ -1,3 +1,22 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Copyright (C) 2013 Natalia Galin.
+% Questions/Comments/Suggestion/Corretions? 
+% Please do get in touch: natalia.galin@gmail.com
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 %CURRENTLY IMPLEMENTED TO GIVE 'EXACTLY' THE SAME VALUES AS THE OCOG
 %RETRACKER IN IDL WOULD. HOWEVER, THIS IS NOT ENTIRELY CORRECT, AS IDL
 %INDEXING STARTS FROM 0 AND MATLAB FROM 1. NG - 25th Jun, 2011
