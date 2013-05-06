bd1630fc3bb1e33db4e9fe470249e54c49a9c929
Natalia Galin <natalia.galin@gmail.com>
2013-05-06 13:06:28 -0300

    UsageExample_v1.m

---
+++ UsageExample_v1.m
@@ -0,0 +1,7 @@
+load ExampleData.mat
+RETRACK_FLAG = 1;
+HAMWIN_FLAG = 0;
+[BURSTPOWER] = CRYOSAT2_FBR2SAR(COMPLEX_DATA_I,COMPLEX_DATA_Q,SAT_VEL,ALT_RATE,ALTITUDE,RETRACK_FLAG,HAMWIN_FLAG);
+%output will be a [50x64x256] matrix of power of beams in every burst
+%average across all bursts, and plot a radargram
+figure;pcolor(squeeze(mean(BURSTPOWER,1)).');shading interp
